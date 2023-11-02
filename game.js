0c76368b18a4cb089b3aea4d3326865fc79ad6f8
V4mpw0L <125161567+V4mpw0L@users.noreply.github.com>
2023-11-01 21:25:14 -0700

    Update game.js

--- game.js
+++ game.js
@@ -5,47 +5,56 @@ document.getElementById('skills').addEventListener('click', function(event) {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
         this.classList.add('clicked');
+        document.getElementById('messageBox').innerHTML += '<p>Você clicou em Skills!</p>';
     } else {
         menu.classList.add('hidden');
         this.classList.remove('clicked');
         skillButtons.forEach(function(button) {
             button.classList.remove('clicked');
         });
+        document.getElementById('messageBox').innerHTML += '<p>Você fechou Skills!</p>';
     }
 });
-document.getElementById('fishing').addEventListener('click', function(event) {
+
+function handleSkillButtonClick(event) {
     event.stopPropagation();
     skillButtons.forEach(function(button) {
         button.classList.remove('clicked');
     });
     this.classList.add('clicked');
-    console.log('Você está pescando!');
-});
-document.getElementById('mining').addEventListener('click', function(event) {
-    event.stopPropagation();
-    skillButtons.forEach(function(button) {
-        button.classList.remove('clicked');
-    });
-    this.classList.add('clicked');
-    console.log('Você está minerando!');
-});
-document.getElementById('crafting').addEventListener('click', function(event) {
-    event.stopPropagation();
-    skillButtons.forEach(function(button) {
-        button.classList.remove('clicked');
-    });
-    this.classList.add('clicked');
-    console.log('Você está fazendo crafting!');
-});
+    document.getElementById('messageBox').innerHTML += '<p>Você está ' + this.id + '!</p>';
+}
+
+document.getElementById('fishing').addEventListener('click', handleSkillButtonClick);
+document.getElementById('mining').addEventListener('click', handleSkillButtonClick);
+document.getElementById('crafting').addEventListener('click', handleSkillButtonClick);
+
+
+
 document.getElementById('combatMenu').addEventListener('click', function() {
-    console.log('Você está em combate!');
+    document.getElementById('messageBox').innerHTML += '<p>Você está em combate!</p>';
 });
 document.getElementById('inventoryMenu').addEventListener('click', function() {
-    console.log('Seu Inventory!');
+    document.getElementById('messageBox').innerHTML += '<p>Seu Inventory!</p>';
+});
+document.getElementById('profileBtn').addEventListener('click', function() {
+    document.getElementById('messageBox').innerHTML += '<p>Seu Perfil!</p>';
 });
 
-document.getElementById('profileBtn').addEventListener('click', function() {
-    console.log('Seu Perfil!');
+document.getElementById('bottomButton1').addEventListener('click', function() {
+    document.getElementById('messageBox').innerHTML += '<p>Button 1!</p>';
+});
+
+document.getElementById('bottomButton2').addEventListener('click', function() {
+    document.getElementById('messageBox').innerHTML += '<p>Button 2!</p>';
+});
+
+document.getElementById('bottomButton3').addEventListener('click', function() {
+    document.getElementById('messageBox').innerHTML += '<p>Button 3!</p>';
+});
+
+document.getElementById('bottomButton4').addEventListener('click', function() {
+    document.getElementById('messageBox').innerHTML += '<p>Button 4!</p>';
 });
 
 document.addEventListener('click', function() {
@@ -58,3 +67,4 @@ document.addEventListener('click', function() {
         });
     }
 });
+
